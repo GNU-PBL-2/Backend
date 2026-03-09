f908fda81506053249d5f071bc0d61971a47430b
HongJongMin <gwa7417@gmail.com>
2026-03-09 17:48:57 +0900

    feat: 기초 셋팅

---
+++ src/test/java/gnu/project/pbl2/Pbl2ApplicationTests.java
@@ -0,0 +1,13 @@
+package gnu.project.pbl2;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Pbl2ApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
