db7c47e85e08d6f19332c44fc4b5cd431795bffa
HongJongMin <gwa7417@gmail.com>
2026-03-21 17:47:14 +0900

    feat: Slf4j 제거

--- src/main/java/gnu/project/pbl2/auth/controller/OauthController.java
+++ src/main/java/gnu/project/pbl2/auth/controller/OauthController.java
@@ -4,13 +4,11 @@ import gnu.project.pbl2.auth.dto.request.OauthLoginRequest;
 import gnu.project.pbl2.auth.dto.response.AuthTokenDto;
 import gnu.project.pbl2.auth.service.OauthService;
 import lombok.RequiredArgsConstructor;
-import lombok.extern.slf4j.Slf4j;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
-@Slf4j
 @RestController
 @RequiredArgsConstructor
 @RequestMapping("api/v1/auth")
